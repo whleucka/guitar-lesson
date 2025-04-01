4165183d3a1ce3277cdee3704140443cae40eaf5
William Hleucka <william.hleucka@gmail.com>
2025-04-01 14:21:14 +0000

    wip: scales template

---
+++ inc/scales.php
@@ -0,0 +1,15 @@
+<ul class="list-group">
+    <li class="list-group-item">
+        <div class="d-flex align-items-center">
+            <label>Play</label>
+            <select id="scale-select" onChange="toggleScale(event)" class="form-control ms-2">
+                <option value="off" selected>Off</option>
+                <?php foreach ($scales as $scale => $data): ?>
+                    <?php foreach ($scales as $scale => $data): ?>
+                        <option value="<?=$scale?>"><?=$scale?></option>
+                    <?php endforeach ?>
+                <?php endforeach ?>
+            </select>
+        </div>
+    </li>
+</ul>
